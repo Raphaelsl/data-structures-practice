15550adbe32bbd0d03c8816e2257f36cdf134f9c
rapha <raphalameira10@gmail.com>
2025-11-23 23:28:31 -0300

    feat: change gitignore and add start linked

--- src/main/java/com/example/datastructures/linkedlists/SinglyLinkedList.java
+++ src/main/java/com/example/datastructures/linkedlists/SinglyLinkedList.java
@@ -1,4 +1,34 @@
 package com.example.datastructures.linkedlists;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node<T> head;
+    private int size;
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    // TODO  Implemente aqui:
+    // - append(T value)
+    // - prepend(T value)
+    // - insertAt(int index, T value)
+    // - remove(T value)
+    // - reverse()
+    // - find(T value)
+    // - hasCycle()
+    // - size()
+    // - isEmpty()
+    // - toList()
 }
